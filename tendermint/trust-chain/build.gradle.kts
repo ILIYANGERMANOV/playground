71710d57c7f03ddfeea32fd1fe5ad6b6dca753bc
Iliyan Germanov <iliyan@qredo.com>
2021-12-21 12:28:35 +0200

    WIP: Tendermint setup

--- tendermint/trust-chain/build.gradle.kts
+++ tendermint/trust-chain/build.gradle.kts
@@ -1,8 +1,22 @@
+import com.google.protobuf.gradle.protobuf
+import com.google.protobuf.gradle.protoc
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.protobuf.gradle.*
 
 plugins {
     kotlin("jvm") version "1.5.10"
     application
+    id("java")
+    id("com.google.protobuf") version "0.8.18"
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.18")
+    }
 }
 
 group = "me.iliyan"
@@ -13,7 +27,27 @@ repositories {
 }
 
 dependencies {
+    implementation("io.grpc:grpc-protobuf:1.42.1")
+    implementation("io.grpc:grpc-netty-shaded:1.42.1")
+    implementation("io.grpc:grpc-stub:1.42.1")
+    
     testImplementation(kotlin("test"))
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.7.1"
+    }
+//    plugins {
+//        grpc {
+//            artifact = "io.grpc:protoc-gen-grpc-java:1.22.1"
+//        }
+//    }
+//    generateProtoTasks {
+//        all()*.plugins {
+//            grpc {}
+//        }
+//    }
 }
 
 tasks.test {
