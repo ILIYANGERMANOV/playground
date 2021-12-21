6a3a15a370a187724710062d484ff349d603ea88
Iliyan Germanov <iliyan@qredo.com>
2021-12-21 12:36:43 +0200

    WIP: Tendermint setup

--- tendermint/trust-chain/build.gradle.kts
+++ tendermint/trust-chain/build.gradle.kts
@@ -38,11 +38,11 @@ protobuf {
     protoc {
         artifact = "com.google.protobuf:protoc:3.7.1"
     }
-//    plugins {
-//        grpc {
-//            artifact = "io.grpc:protoc-gen-grpc-java:1.22.1"
-//        }
-//    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.22.1"
+        }
+    }
 //    generateProtoTasks {
 //        all()*.plugins {
 //            grpc {}
